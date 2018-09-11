c06c1be691266ed0cefdce407cd9928e444900af
Kinyanjui Wangonya <wangonya@ip-184-168-28-103.ip.secureserver.net>
2018-09-11 18:06:20 +0300

    update about

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
-      <div class="row">
-        <p class="column">about fora here</p>
-        <p class="column">more details here</p>
+      <div>
+        <div>The Lord has a place for every one in His great plan. Talents that are not needed are not bestowed.—9T 37.3</div>
+        <div>Fora is where Seventh-day Adventist Christians share ideas and help each other grow. It is an online community for sharing and discovering great ideas, having healthy discussions, and making friends. Anyone can share articles, questions, discussions, etc. as long as they are in line with our statement of faith.</div>
+        <div class="">It is not restricted to Seventh-day Adventists. Visitors are welcome to browse articles, ask questions, and get to know more about our faith</div>
+        <div class="">We strive to be the most friendly, kind and welcoming christian community online</div>
+        <div class="">The right culture and use of the power of speech has to do with every line of Christian work; it enters into the home life, and into all our intercourse with one another. We should accustom ourselves to speak in pleasant tones, to use pure and correct language, and words that are kind and courteous. Sweet, kind words are as dew and gentle showers to the soul. The Scripture says of Christ that grace was poured into His lips that He might "know how to speak a word in season to him that is weary." Ps. 45:2; Isa. 50:4. And the Lord bids us, "Let your speech be alway with grace" (Col. 4:6) "that it may minister grace unto the hearers" (Eph. 4:29). [COL p. 337]</div>
       </div>
     </div>
 </template>
