940a64fd1723293a50d38b046d7f17d2ed356c7d
Kinyanjui Wangonya <kwangonya@gmail.com>
2018-09-14 17:23:18 +0300

    write to firestore

--- pages/new/index.vue
+++ pages/new/index.vue
@@ -1,12 +1,23 @@
 <template>
     <div>
       new post here
+      <div>
+        <button @click="write">
+          <span v-if="write">Write now</span>
+          <span v-else>Successful!</span>
+        </button>
+      </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "index"
+        name: "index",
+        methods: {
+          write() {
+            this.$store.dispatch('writeToFirestore')
+          }
+        }
     }
 </script>
 
--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
 import Vuex from 'vuex'
-import firebase, {auth} from '@/services/fireinit.js'
+import firebase, {auth, StoreDB} from '@/services/fireinit.js'
 
 const createStore = () => {
   return new Vuex.Store({
@@ -19,6 +19,20 @@ const createStore = () => {
     actions: {
       autoSignIn ({commit}, payload) {
         commit('setUser', payload)
+      },
+
+      async writeToFirestore() {
+        const ref = StoreDB.collection("test").doc("test")
+        const document = {
+          text: "This is a test message."
+        }
+
+        try {
+          await ref.set(document)
+        } catch (e) {
+          // TODO: error handling
+          console.error(e)
+        }
       },
 
       signOut ({commit}) {
