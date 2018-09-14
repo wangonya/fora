b2f5e4c438ce6b3bcfd61bd01086126d371aa991
Kinyanjui Wangonya <kwangonya@gmail.com>
2018-09-14 13:50:28 +0300

    consume from component

---
+++ components/LoginForm.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title">
+        Login
+      </p>
+      <a href="#" class="card-header-icon" aria-label="more options">
+      <span class="icon">
+        <i class="fas fa-angle-down" aria-hidden="true"></i>
+      </span>
+      </a>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        <b-field>
+          <b-input placeholder="Enter your email"
+                   type="email"
+                   v-model="email"
+                   icon="email">
+          </b-input>
+        </b-field>
+        <b-field>
+          <b-input type="password"
+                   placeholder="Enter your password"
+                   v-model="password"
+                   icon="key"
+                   password-reveal>
+          </b-input>
+        </b-field>
+        <a href="#" class="is-info button is-outlined" v-on:click="login">Login</a>
+      </div>
+    </div>
+    <nuxt-link to="/register">Or create an account</nuxt-link>
+    <br><br>
+  </div>
+</template>
+
+<script>
+  import firebase from 'firebase'
+  export default {
+    name: "LoginForm",
+    data: function () {
+      return {
+        email: '',
+        password: '',
+        loadingComponent: ''
+      }
+    },
+    methods : {
+      login: function () {
+        this.loadingComponent = this.$loading.open()
+        firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+          .then(
+            () => this.$router.replace({ path: '/feed' }),
+            function(error) {
+              // Handle Errors here.
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              if (errorCode == 'auth/weak-password') {
+                alert('The password is too weak.')
+              } else {
+                alert(errorMessage)
+              }
+              console.log(error)
+            }
+          )
+          .then(() => this.loadingComponent.close())
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/RegisterForm.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title">
+        Register
+      </p>
+      <a href="#" class="card-header-icon" aria-label="more options">
+      <span class="icon">
+        <i class="fas fa-angle-down" aria-hidden="true"></i>
+      </span>
+      </a>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        <b-field>
+          <b-input placeholder="Username"
+                   required
+                   type="text"
+                   icon="account">
+          </b-input>
+        </b-field>
+        <b-field>
+          <b-input placeholder="Enter your email"
+                   type="email"
+                   required
+                   v-model="email"
+                   icon="email">
+          </b-input>
+        </b-field>
+        <b-field>
+          <b-input type="password"
+                   placeholder="Enter your password"
+                   icon="key"
+                   required
+                   v-model="password"
+                   password-reveal>
+          </b-input>
+        </b-field>
+        <a href="#" class="is-info button is-outlined" v-on:click="register">Register</a>
+      </div>
+    </div>
+    <nuxt-link to="/login">Go back to login</nuxt-link>
+    <br><br>
+  </div>
+</template>
+
+<script>
+  import firebase from 'firebase'
+  export default {
+        name: "RegisterForm",
+        data: function () {
+          return {
+            email: '',
+            password: '',
+            loadingComponent: ''
+          }
+        },
+        methods : {
+          register: function () {
+            this.loadingComponent = this.$loading.open()
+            firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+              .then(
+                () => this.$router.replace({ path: '/feed' })
+              )
+              .catch(function(error) {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                if (errorCode == 'auth/weak-password') {
+                  alert('The password is too weak.')
+                } else {
+                  alert(errorMessage)
+                }
+                console.log(error)
+              })
+              .then(() => this.loadingComponent.close())
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
